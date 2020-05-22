dee47ca23dc62335e6f15fe641cf7535fed14bf8
jujien <vukien104@icloud.com>
2020-05-22 14:19:41 +0700

    Initial Commit

---
+++ TestNFC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestNFC
+//
+//  Created by jujien on 5/22/20.
+//  Copyright © 2020 fitden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
